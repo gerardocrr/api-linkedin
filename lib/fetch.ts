08ce2cd69f5da2499d0d3df644f9fbeacc2fbd55
gerardocrr <gerardo2551.cr@gmail.com>
2024-07-13 16:06:00 -0600

    changes jobs location

--- lib/fetch.ts
+++ lib/fetch.ts
@@ -8,7 +8,9 @@ export const jobData = async (id: string) => {
   const $ = cheerio.load(html);
   const title = $("h1.top-card-layout__title").text();
   const company = $("a.topcard__org-name-link").text().trim();
-  const location = $("span.topcard__flavor--bullet").text().trim();
+  const location = $("span.topcard__flavor.topcard__flavor--bullet")
+    .text()
+    .trim();
   const postedTimeAgo = $("span.posted-time-ago__text").text().trim();
   const description = $("div.show-more-less-html__markup").text().trim();
   const jobCriteria: string[] = [];
