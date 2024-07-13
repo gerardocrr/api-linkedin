51b461c090f60aa2201537852b565d906c7fcf6a
gerardocrr <gerardo2551.cr@gmail.com>
2024-07-12 22:30:02 -0600

    add cheerio

--- api/index.ts
+++ api/index.ts
@@ -1,14 +1,32 @@
-import { Hono } from 'hono'
-import { handle } from 'hono/vercel'
+import { Hono } from "hono";
+import { handle } from "hono/vercel";
+import * as cheerio from "cheerio";
 
 export const config = {
-  runtime: 'edge'
+  runtime: "edge",
+};
+
+const app = new Hono().basePath("/api");
+
+async function datos() {
+  const response = await fetch(
+    "https://www.linkedin.com/jobs/view/3966977311/"
+  );
+  const html = await response.text();
+
+  const $ = cheerio.load(html);
+  const titulo = $("h1.top-card-layout__title").text();
+
+  return titulo;
 }
 
-const app = new Hono().basePath('/api')
+app.get("/", async (c) => {
+  const data = await datos();
+  return c.text(data);
+});
 
-app.get('/', (c) => {
-  return c.json({ message: 'Hello Hono!' })
-})
+app.get("/", (c) => {
+  return c.json({ message: "Welcome to the unofficial Linkedin API!" });
+});
 
-export default handle(app)
+export default handle(app);
